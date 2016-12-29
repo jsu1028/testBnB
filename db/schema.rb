ce7fa5f047036edd8975f6727f406d6031d06984
Joshua Su <jsu1028@ymail.com>
2016-12-29 18:50:24 +0800

    added image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161228083103) do
+ActiveRecord::Schema.define(version: 20161229075753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20161228083103) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.json     "photos"
     t.index ["user_id"], name: "index_listings_on_user_id", using: :btree
   end
 
@@ -56,6 +57,7 @@ ActiveRecord::Schema.define(version: 20161228083103) do
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128, null: false
     t.string   "name"
+    t.json     "avatars"
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
